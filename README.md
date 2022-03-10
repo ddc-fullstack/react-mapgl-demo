dfdfbd83b2f888949585508ad683915329c885f3
George Kephart <gkephart@cnm.edu>
2022-03-10 16:27:41 -0700

    updated the react-mapgl demo to work on version 7

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 ## Package used: [React Map GL](https://github.com/visgl/react-map-gl)
 ## How to add React Map GL
 1. Sign up for an account with mapbox and get a mapbox access token.
-1. Create `/frontend/.env` and set REACT_APP_MAPBOX_ACCESS_TOKEN= to equal your access token (Make sure `/frontend.env` is gitignored)    
-1. cd into `/frontend`  and run `npm install --save react-map-gl`
+1. Create `/frontend/.env` and set REACT_APP_MAPBOX_ACCESS_TOKEN= to equal your access token (Make sure `/frontend/.env` is gitignored)    
+1. cd into `/frontend`  and run `npm install --save react-map-gl mapbox-gl`
 1. make sure to add `import 'mapbox-gl/dist/mapbox-gl.css'` to `/frontend/ui/App.js`
 ## 
 
--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -1,39 +1,37 @@
-import React from "react"
-import ReactMapGL from 'react-map-gl';
-import {Container} from "react-bootstrap";
-import {Pin} from "./Pin";
+import React from 'react'
+import Map from 'react-map-gl'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Pin } from './Pin'
 
-export function Home() {
-	const [points, setPoints] = React.useState([
-		{lat: 35.332, lng: -106.652},
-		{lat: 35.339, lng: -106.656},
-		{lat: 35.40, lng: -106.666},
-		{lat: 35.23, lng: -106.4444}
-	]);
-	// center={[-106.65, 35.33]}
+export function Home () {
+  const [points] = React.useState([
+    { lat: 35.332, lng: -106.652 },
+    { lat: 35.339, lng: -106.656 },
+    { lat: 35.40, lng: -106.666 },
+    { lat: 35.23, lng: -106.4444 }
+  ])
 
-	const [viewport, setViewport] = React.useState({
-		latitude: 35.33,
-		longitude: -106.65,
-		zoom: 12
-	});
+  return (
+    <>
+      <Container>
+        <Row className={"justify-content-center"}>
+          <Col>
+            <h1>Here is the map</h1>
+            <Map
+              initialViewState={{
+                latitude: 35.33,
+                longitude: -106.65,
+                zoom: 9
+              }}
+              style={{ width: 600, height: 400 }}
+              mapStyle="mapbox://styles/mapbox/dark-v9"
+            >
+              {points.map((point, index) => <Pin lat={point.lat} lng={point.lng} index={index} key={index}/>)}
+            </Map>
+          </Col>
+        </Row>
+      </Container>
 
-	return (
-		<>
-			<Container>
-				<h1>Here is the map</h1>
-				<ReactMapGL
-					{...viewport}
-					width="50vw"
-					height="50vh"
-					onViewportChange={(viewport) => setViewport(viewport)}
-					mapStyle="mapbox://styles/mapbox/dark-v9"
-				>
-					{points.map((point, index) => <Pin lat={point.lat} lng={point.lng} index={index} key={index}/>)}
-				</ReactMapGL>
-
-			</Container>
-
-		</>
-	)
+    </>
+  )
 }
